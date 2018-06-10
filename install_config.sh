588f72db1246d40948152ea9e9b1768ec6203fe7
Teodor Draganov <draganov93@gmail.com>
2018-06-10 14:07:58 +0000

    Add basic install script

---
+++ install_config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get directory of current script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Vim config
+ln -s $DIR/.vimrc $HOME/.vimrc
+ln -s $DIR/.vim   $HOME/.vim
+vim -c PlugInstall -c qa
+
+# Bash resources
+echo >> $HOME/.bashrc
+echo "source $DIR/.custom_bashrc.sh" >> $HOME/.bashrc
+echo "source $DIR/.ruby_bashrc.sh"   >> $HOME/.bashrc
+echo "source $DIR/.git_bashrc.sh"    >> $HOME/.bashrc
+
+# Install and configure bash-git-prompt
+git clone https://github.com/magicmonty/bash-git-prompt $HOME/.bash-git-prompt
+ln -s $DIR/.git-prompt-colors.sh $HOME/.git-prompt-colors.sh
+
+# Others
+ln -s $DIR/.tmux.conf        $HOME/.tmux.conf
+ln -s $DIR/.tmux-themepack   $HOME/.tmux-themepack
+ln -s $DIR/.gitconfig        $HOME/.gitconfig
+ln -s $DIR/.inputrc          $HOME/.inputrc
+ln -s $DIR/.config/alacritty $HOME/.config/alacritty
+ln -s $DIR/.config/nvim      $HOME/.config/nvim
