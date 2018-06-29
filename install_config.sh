fc65df39657cbb5d02c39afa9ed6b4e8f1d08aa9
Teodor Draganov <tdragano@seivdilc60429.eu.asml.com>
2018-06-29 11:03:13 +0200

    Refactor install_script.sh

--- install_config.sh
+++ install_config.sh
@@ -3,31 +3,33 @@
 # Get directory of current script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Vim config
-ln -s $DIR/.vimrc $HOME/.vimrc
-ln -s $DIR/.vim   $HOME/.vim
+# (N)Vim config
+ln -s $DIR/.vimrc       $HOME/.vimrc
+ln -s $DIR/.vim         $HOME/.vim
+ln -s $DIR/.config/nvim $HOME/.config/nvim
 vim -c PlugInstall -c qa
 
-# Bash resources
+# Tmux config
+mkdir -p $DIR/.tmux/plugins
+ln -s $DIR/.tmux.conf      $HOME/.tmux.conf
+ln -s $DIR/.tmux           $HOME/.tmux
+ln -s $DIR/.tmux-themepack $HOME/.tmux-themepack
+
+# Bash config
 echo >> $HOME/.bashrc
 echo "source $DIR/.custom_bashrc.sh" >> $HOME/.bashrc
 echo "source $DIR/.ruby_bashrc.sh"   >> $HOME/.bashrc
 echo "source $DIR/.git_bashrc.sh"    >> $HOME/.bashrc
+git clone https://github.com/magicmonty/bash-git-prompt $HOME/.bash-git-prompt
+ln -s $DIR/.git-prompt-colors.sh $HOME/.git-prompt-colors.sh
 
 # Others
-ln -s $DIR/.tmux.conf        $HOME/.tmux.conf
-ln -s $DIR/.tmux             $HOME/.tmux
-ln -s $DIR/.tmux-themepack   $HOME/.tmux-themepack
 ln -s $DIR/.gitconfig        $HOME/.gitconfig
 ln -s $DIR/.inputrc          $HOME/.inputrc
 ln -s $DIR/.config/alacritty $HOME/.config/alacritty
-ln -s $DIR/.config/nvim      $HOME/.config/nvim
 
 # Create local structure
 mkdir -p $HOME/local/bin
 mkdir -p $HOME/local/tools
 ln -s $HOME/local/bin $HOME/bin
 
-# Install and configure bash-git-prompt
-git clone https://github.com/magicmonty/bash-git-prompt $HOME/.bash-git-prompt
-ln -s $DIR/.git-prompt-colors.sh $HOME/.git-prompt-colors.sh
