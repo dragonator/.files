62d7d1f395d77cbd7ed8d6765c18b4cdcf2fe04a
Teodor Draganov <draganov93@gmail.com>
2018-12-28 12:07:05 +0200

    [ruby] Add rbenv initialization

--- install_config.sh
+++ install_config.sh
@@ -19,6 +19,7 @@ ln -s $DIR/.tmux-themepack $HOME/.tmux-themepack
 echo >> $HOME/.bashrc
 echo "eval \"\$(jump shell)\""       >> $HOME/.bashrc
 echo "eval \"\$(thefuck --alias)\""  >> $HOME/.bashrc
+echo "eval \"\$(rbenv init -)\""     >> $HOME/.bashrc
 echo "source $DIR/.custom_bashrc.sh" >> $HOME/.bashrc
 echo "source $DIR/.ruby_bashrc.sh"   >> $HOME/.bashrc
 echo "source $DIR/.git_bashrc.sh"    >> $HOME/.bashrc
