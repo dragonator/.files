98a5b384c4f1fd6137f952d90711ede3d419334d
Teodor Draganov <draganov93@gmail.com>
2019-08-25 02:43:16 +0300

    [installer] Install applications; Make output more organized

--- install_config.sh
+++ install_config.sh
@@ -13,7 +13,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $HOME/code
 ls -sTf $DIR/inputrc $HOME/.inputrc
 
-echo "Sourcing master.bashrc.sh"
+echo "=> Sourcing master.bashrc.sh"
 SOURCE_MASTER_BASHRC="""
 # Source custom configuration
 source $HOME/.files/master.bashrc.sh"""
@@ -21,22 +21,24 @@ if ! grep "master.bashrc.sh" $HOME/.bashrc ; then
     echo "$SOURCE_MASTER_BASHRC" >> $HOME/.bashrc
 fi
 
+echo
 ##############################################################
 ##                          (N)Vim                          ##
 ##############################################################
-echo "Configuring Vim"
+echo "=> Configuring Vim"
 
 # (N)Vim config
 ln -sTf $DIR/vimrc $HOME/.vimrc
 ln -sTf $DIR/vim   $HOME/.vim
 #ln -sTf $DIR/config/nvim $HOME/.config/nvim
-echo "Installing Vim plugins"
+echo "=> Installing Vim plugins"
 vim -c PlugInstall -c qa
 
+echo
 ##############################################################
 ##                           Tmux                           ##
 ##############################################################
-echo "Configuring Tmux"
+echo "=> Configuring Tmux"
 
 # Create symbolic links from $HOME to Tmux configuration files
 ln -sTf $DIR/tmux.conf      $HOME/.tmux.conf
@@ -44,28 +46,44 @@ ln -sTf $DIR/tmux           $HOME/.tmux
 ln -sTf $DIR/tmux-themepack $HOME/.tmux-themepack
 
 # Install TPM (Tmux Plugin Manager)
-echo "Installing TPM"
+echo "=> Installing TPM"
 TMUX_PLUGIN_DIR="$DIR/tmux/plugins"
 mkdir -p $TMUX_PLUGIN_DIR
 git clone https://github.com/tmux-plugins/tpm $TMUX_PLUGIN_DIR/tpm
 
 # Install TPM plugins
-echo "Installing TPM Plugins"
+echo "=> Installing TPM Plugins"
 tmux start-server      # start a server but don't attach to it
 tmux new-session -d    # create a new session but don't attach to it either
 $TMUX_PLUGIN_DIR/tpm/scripts/install_plugins.sh   # install the plugins
 tmux kill-server       # killing the server is not required, I guess
 
+echo
 ##############################################################
 ##                            Git                           ##
 ##############################################################
-echo "Configuring Git"
+echo "=> Configuring Git"
 ln -sTf $DIR/gitconfig $HOME/.gitconfig
 
 # Installing bash-git-prompt
-echo "Installing bash-git-prompt"
+echo "=> Installing bash-git-prompt"
 git clone https://github.com/magicmonty/bash-git-prompt $DIR/bash-git-prompt
 ln -sTf $DIR/bash-git-prompt      $HOME/.bash-git-prompt
 ln -sTf $DIR/git-prompt-colors.sh $HOME/.git-prompt-colors.sh
 
-echo "Finished."
+echo
+##############################################################
+##                    Install applications                  ##
+##############################################################
+echo "=> Installing vim..."
+sudo apt-get install --assume-yes vim   ; echo
+echo "=> Installing tmux..."
+sudo apt-get install --assume-yes tmux  ; echo
+echo "=> Installing git..."
+sudo apt-get install --assume-yes git   ; echo
+echo "=> Installing rbenv..."
+sudo apt-get install --assume-yes rbenv ; echo
+echo "=> Installing xclip..."
+sudo apt-get install --assume-yes xclip ; echo
+
+echo "=] Finished."
