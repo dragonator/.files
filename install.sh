26af94eabc004e17203c8d25d9c141e60da4dae1
Teodor Draganov <draganov93@gmail.com>
2019-08-25 13:36:16 +0300

    [install] Require sudo rights to run the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Require sudo rights
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
 ##############################################################
 ##                      Miscellaneous                       ##
 ##############################################################
