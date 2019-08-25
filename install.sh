1edf261af403f58738fde2d37ff7923ddb872c5a
Teodor Draganov <draganov93@gmail.com>
2019-08-26 00:38:09 +0300

    [shell] Fix shellcheck violations

--- install.sh
+++ install.sh
@@ -20,9 +20,9 @@ echo "-> Updating apt cache"
 sudo apt update
 echo
 
-for pkg in ${PACKAGES[@]} ${RUBY_BUILD_DEPS[@]}; do
+for pkg in "${PACKAGES[@]}" "${RUBY_BUILD_DEPS[@]}"; do
   echo "-> Installing ${pkg}"
-  sudo apt install --assume-yes $pkg
+  sudo apt install --assume-yes "$pkg"
   echo
 done
 
@@ -34,11 +34,11 @@ echo
 ##                          (N)Vim                          ##
 ##############################################################
 echo "=> Configuring Vim"
-ln -sTfv $DIR/vimrc $HOME/.vimrc
-ln -sTfv $DIR/vim   $HOME/.vim
+ln -sTfv "$DIR/vimrc" "$HOME/.vimrc"
+ln -sTfv "$DIR/vim"   "$HOME/.vim"
 echo
 
-#ln -sTfv $DIR/config/nvim $HOME/.config/nvim
+#ln -sTfv "$DIR/config/nvim" "$HOME/.config/nvim"
 echo "-> Installing Vim plugins"
 vim -c PlugInstall -c qa
 echo
@@ -49,25 +49,25 @@ echo
 echo "=> Configuring Tmux"
 TMUX_PLUGIN_DIR="$DIR/tmux/plugins"
 TPM_DIR="$TMUX_PLUGIN_DIR/tpm"
-ln -sTfv $DIR/tmux.conf      $HOME/.tmux.conf
-ln -sTfv $DIR/tmux           $HOME/.tmux
-ln -sTfv $DIR/tmux-themepack $HOME/.tmux-themepack
+ln -sTfv "$DIR/tmux.conf"      "$HOME/.tmux.conf"
+ln -sTfv "$DIR/tmux"           "$HOME/.tmux"
+ln -sTfv "$DIR/tmux-themepack" "$HOME/.tmux-themepack"
 echo
 
 echo "-> Installing TPM"
-mkdir -pv $TMUX_PLUGIN_DIR
-git clone https://github.com/tmux-plugins/tpm $TPM_DIR
+mkdir -pv "$TMUX_PLUGIN_DIR"
+git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 echo
 
 echo "-> Updating TPM"
-git -C $TPM_DIR pull
+git -C "$TPM_DIR" pull
 echo
 
 # Install TPM plugins
 echo "-> Installing TPM Plugins"
 tmux start-server      # start a server but don't attach to it
 tmux new-session -d    # create a new session but don't attach to it either
-$TMUX_PLUGIN_DIR/tpm/scripts/install_plugins.sh   # install the plugins
+"$TMUX_PLUGIN_DIR/tpm/scripts/install_plugins.sh"   # install the plugins
 tmux kill-server       # killing the server is not required, I guess
 echo
 
@@ -75,19 +75,19 @@ echo
 ##                            Git                           ##
 ##############################################################
 echo "=> Configuring Git"
-ln -sTfv $DIR/gitconfig $HOME/.gitconfig
-ln -sTfv $DIR/git-prompt-colors.sh $HOME/.git-prompt-colors.sh
+ln -sTfv "$DIR/gitconfig" "$HOME/.gitconfig"
+ln -sTfv "$DIR/git-prompt-colors.sh" "$HOME/.git-prompt-colors.sh"
 echo
 
 # Installing bash-git-prompt
 BASH_GIT_PROMPT_DIR="$DIR/bash-git-prompt"
 echo "-> Installing bash-git-prompt"
-git clone https://github.com/magicmonty/bash-git-prompt $BASH_GIT_PROMPT_DIR
-ln -sTfv $DIR/bash-git-prompt      $HOME/.bash-git-prompt
+git clone https://github.com/magicmonty/bash-git-prompt "$BASH_GIT_PROMPT_DIR"
+ln -sTfv "$DIR/bash-git-prompt" "$HOME/.bash-git-prompt"
 echo
 
 echo "-> Updating bash-git-prompt"
-git -C $BASH_GIT_PROMPT_DIR pull
+git -C "$BASH_GIT_PROMPT_DIR" pull
 echo
 
 ##############################################################
@@ -100,13 +100,13 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
   launch=n
 echo
 
-mkdir -pv $HOME/bin
-ln -sTfv  $DIR/config/kitty                 $HOME/.config/kitty
-ln -sTfv  $HOME/.local/kitty.app/bin/kitty  $HOME/bin/kitty
-cp -v     $DIR/config/kitty/kitty.desktop   $LOCAL_APPLICATIONS/kitty.desktop
-sed -i    "s/USER/$USER/g"                  $LOCAL_APPLICATIONS/kitty.desktop
-cp -v     $LOCAL_APPLICATIONS/kitty.desktop $HOME/.local/kitty.app/share/applications/
-chmod -v  u+x $LOCAL_APPLICATIONS/kitty.desktop
+mkdir -pv "$HOME/bin"
+ln -sTfv  "$DIR/config/kitty"                 "$HOME/.config/kitty"
+ln -sTfv  "$HOME/.local/kitty.app/bin/kitty"  "$HOME/bin/kitty"
+cp -v     "$DIR/config/kitty/kitty.desktop"   "$LOCAL_APPLICATIONS/kitty.desktop"
+sed -i    "s/USER/$USER/g"                  "$LOCAL_APPLICATIONS/kitty.desktop"
+cp -v     "$LOCAL_APPLICATIONS/kitty.desktop" "$HOME/.local/kitty.app/share/applications/"
+chmod -v  u+x "$LOCAL_APPLICATIONS/kitty.desktop"
 echo
 
 ##############################################################
@@ -118,30 +118,30 @@ RBENV_DIR="$DIR/rbenv"
 RUBY_BUILD_DIR="$RBENV_DIR/plugins/ruby-build"
 
 echo "-> Installing rbenv"
-git clone https://github.com/rbenv/rbenv.git $RBENV_DIR
-ln -sTfv $RBENV_DIR $HOME/.rbenv
+git clone https://github.com/rbenv/rbenv.git "$RBENV_DIR"
+ln -sTfv "$RBENV_DIR" "$HOME/.rbenv"
 echo
 
 echo "-> Updating rbenv"
-git -C $RBENV_DIR pull
+git -C "$RBENV_DIR" pull
 echo
 
 echo "-> Installing ruby-build"
-git clone https://github.com/rbenv/ruby-build.git $RUBY_BUILD_DIR
+git clone https://github.com/rbenv/ruby-build.git "$RUBY_BUILD_DIR"
 echo
 
 echo "-> Updating ruby-build"
-git -C $RUBY_BUILD_DIR pull
+git -C "$RUBY_BUILD_DIR" pull
 echo
 
 echo "-> Installing Ruby v$RUBY_VERSION"
-$RBENV_DIR/bin/rbenv install --skip-existing $RUBY_VERSION
-$RBENV_DIR/bin/rbenv global $RUBY_VERSION
+"$RBENV_DIR/bin/rbenv" install --skip-existing "$RUBY_VERSION"
+"$RBENV_DIR/bin/rbenv" global "$RUBY_VERSION"
 echo
 
 echo "-> Installing gems"
-$RBENV_DIR/shims/gem install bundler
-$RBENV_DIR/shims/gem install rubocop
+"$RBENV_DIR/shims/gem" install bundler
+"$RBENV_DIR/shims/gem" install rubocop
 echo
 
 ##############################################################
@@ -152,14 +152,14 @@ SOURCE_MASTER_BASHRC="""
 source $HOME/.files/master.bashrc.sh"""
 
 echo "=> Sourcing master.bashrc.sh"
-if ! grep "master.bashrc.sh" $HOME/.bashrc ; then
-    echo "$SOURCE_MASTER_BASHRC" >> $HOME/.bashrc
+if ! grep "master.bashrc.sh" "$HOME/.bashrc" ; then
+    echo "$SOURCE_MASTER_BASHRC" >> "$HOME/.bashrc"
 fi
 echo
 
 echo "=> Miscellaneous"
-mkdir -pv $HOME/code
-ln -sTfv $DIR/inputrc $HOME/.inputrc
+mkdir -pv "$HOME/code"
+ln -sTfv "$DIR/inputrc" "$HOME/.inputrc"
 echo
 
 ##############################################################
--- uninstall.sh
+++ uninstall.sh
@@ -10,35 +10,35 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Remove links at HOME
 echo "-> Removing links from $HOME"
-rm -fv $HOME/.vimrc
-rm -fv $HOME/.vim
-rm -fv $HOME/.tmux.conf
-rm -fv $HOME/.tmux
-rm -fv $HOME/.tmux-themepack
-rm -fv $HOME/.gitconfig
-rm -fv $HOME/.git-prompt-colors.sh
-rm -fv $HOME/.bash-git-prompt
-rm -fv $HOME/.config/kitty
-rm -fv $HOME/bin/kitty
-rm -fv $HOME/.rbenv
-rm -fv $HOME/.inputrc
+rm -fv "$HOME/.vimrc"
+rm -fv "$HOME/.vim"
+rm -fv "$HOME/.tmux.conf"
+rm -fv "$HOME/.tmux"
+rm -fv "$HOME/.tmux-themepack"
+rm -fv "$HOME/.gitconfig"
+rm -fv "$HOME/.git-prompt-colors.sh"
+rm -fv "$HOME/.bash-git-prompt"
+rm -fv "$HOME/.config/kitty"
+rm -fv "$HOME/bin/kitty"
+rm -fv "$HOME/.rbenv"
+rm -fv "$HOME/.inputrc"
 
 # Delete configuration
 echo "-> Deleting configurations"
-rm -fv  $HOME/.local/share/applications/kitty.desktop
-rm -rfv $HOME/.local/kitty.app
-rm -rfv $DIR/vim/plugs
-rm -rfv $DIR/tmux/plugins
-rm -rfv $DIR/bash-git-prompt
-rm -rfv $DIR/rbenv
+rm -fv  "$HOME/.local/share/applications/kitty.desktop"
+rm -rfv "$HOME/.local/kitty.app"
+rm -rfv "$DIR/vim/plugs"
+rm -rfv "$DIR/tmux/plugins"
+rm -rfv "$DIR/bash-git-prompt"
+rm -rfv "$DIR/rbenv"
 
 # Uninstall packages
 PACKAGES=(git tree vim tmux xclip fonts-firacode curl)
 RUBY_BUILD_DEPS=(gcc make libssl-dev libreadline-dev zlib1g-dev)
 
-for pkg in ${PACKAGES[@]} ${RUBY_BUILD_DEPS[@]}; do
+for pkg in "${PACKAGES[@]}" "${RUBY_BUILD_DEPS[@]}" ; do
   echo "-> Removing ${pkg}"
-  sudo apt install --assume-yes $pkg
+  sudo apt install --assume-yes "$pkg"
   echo
 done
 
