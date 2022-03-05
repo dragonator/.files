f443fd2f7318659fd6884c4e9c5d7fdba95e6ef2
Teodor Draganov <draganov93@gmail.com>
2022-03-06 01:19:32 +0200

    Load bash completions if available

--- bash/master.bashrc.sh
+++ bash/master.bashrc.sh
@@ -23,6 +23,16 @@ export GOTESTSUM_FORMAT="testname"
 ##############################################################
 ##                Includes and integrations                 ##
 ##############################################################
+
+# enable bash completion in interactive shells
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
 # Get directory of current script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
